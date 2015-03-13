42fa4936060744489a63196205e1834619ae8e98
Oleksandr Samoylyk <oleksandr.samoylyk@edu.campus02.at>
2015-03-13 16:48:13 +0100

    längsterString umgeschrieben

--- src/Listen.hs
+++ src/Listen.hs
@@ -20,11 +20,11 @@ längeS (x:xs) = 1 + längeS(xs)
 -- Beispiel: längsterString ["ein", "zwei", "dreizehn", "zwölf"] == "dreizehn"
 -- Punkte: 3
 längsterString :: [String] -> String
-längsterString xs = g xs ""
-  where g [] s = s
-        g (x:xs) s
-          | längeS x > längeS s = g xs x
-          | otherwise = g xs s
+längsterString [] = ""
+längsterString (x:xs)
+          | längeS x > längeS y = x
+          | otherwise = y
+          where y = längsterString xs
 {- Tipp: Bei dieser Funktion ist eine Hilfsvariable bzw. ein Akkumulator nützlich.
          Merken Sie sich den längsten String (und optional dessen Länge) und
          vergleichen Sie den mit dem aktuellen String.
