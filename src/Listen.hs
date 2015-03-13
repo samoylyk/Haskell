f288d9e775d15860b6763a9c155df30ad5651ea7
Oleksandr Samoylyk <oleksandr.samoylyk@edu.campus02.at>
2015-03-13 17:00:04 +0100

    nimm umgeschrieben

--- src/Listen.hs
+++ src/Listen.hs
@@ -36,9 +36,9 @@ längsterString (x:xs)
 -- Beispiel: nimm 3 "ABCDEF" == "ABC"; nimm 3 "AB" == "AB"; nimm 3 [] == []
 -- Punkte: 2
 nimm :: Int -> [Char] -> [Char]
+nimm 0 _ = []
 nimm _ [] = []
-nimm n _ | n <= 0 = []
-nimm n (x:xs) = x : nimm (n-1) xs
+nimm n (x:xs) | n > 0 = x : nimm (n-1) xs
 
 
 -- Löscht die ersten n Zeichen einer Liste. Ist die Liste kürzer als die angegebene
